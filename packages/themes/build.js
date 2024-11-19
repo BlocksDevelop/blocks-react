10308c7dafcb614eb609bbb68a18625982c81d78
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-19 20:58:51 +0900

    refactor: cjs, esm형식 빌드 병렬로 처리되도록 promise.all 적용

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,20 +1,24 @@
 import esbuild from "esbuild";
 
-esbuild.build({
-  entryPoints: ["./src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outfile: "dist/index.js",
-  format: "esm",
-});
-
-esbuild.build({
-  entryPoints: ["./src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outfile: "dist/index.cjs",
-  format: "cjs",
-  outExtension: { ".js": ".mjs" },
+Promise.all([
+  esbuild.build({
+    entryPoints: ["./src/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: "dist/index.js",
+    format: "esm",
+  }),
+  esbuild.build({
+    entryPoints: ["./src/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: "dist/index.cjs",
+    format: "cjs",
+    outExtension: { ".js": ".mjs" },
+  }),
+]).catch(() => {
+  console.error("Build failed");
+  process.exit(1);
 });
