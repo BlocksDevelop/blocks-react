0726cb45f0f0261afe9ec80b155256046e0c8e55
wook <yho7955@naver.com>
2024-11-24 17:43:27 +0900

    chore: 스토리북 설치

---
+++ packages/storybook/stories/components/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import '@blocks/react-components-layout/style.css';
+import { Block as _Block } from '@blocks/react-components-layout';
+
+export default {
+  title: 'components/Layout/Block',
+  component: _Block,
+  parameter: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const BlockStory = {};
